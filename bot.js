0006c0044272912396c31f8658e8297992ee51c7
stokesy#3377 <meetummusicdirrctor@gmail.com>
2026-06-03 17:34:31 +0530

    upgrade whatsapp-web.js to github main branch and remove hardcoded webVersionCache

--- bot.js
+++ bot.js
@@ -455,10 +455,6 @@ async function connectToWhatsApp() {
     authStrategy: new LocalAuth(),
     puppeteer: puppeteerConfig,
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
-    webVersionCache: {
-      type: 'remote',
-      remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html'
-    }
   });
 
   client.on('qr', (qr) => {
